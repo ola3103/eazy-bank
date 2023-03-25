89291fb4d41edb9688f1b14a645c94aae0cf71f1
ola3103 <wonuola.og31@yahoo.com>
2023-03-25 15:02:14 +0100

    Section two added

--- src/section-1.jsx
+++ src/section-1.jsx
@@ -1,8 +1,14 @@
 const SectionOne = () => {
   return (
-    <div className="dashboard-box">
-      <img src="/dashboard.png" />
-    </div>
+    <section className="sponsor">
+      <div className="sponsor-box ">
+        <img src="/sponsor-1.png" alt="sponsor" />
+        <img src="/sponsor-2.png" alt="sponsor" />
+        <img src="/sponsor-3.png" alt="sponsor" />
+        <img src="/sponsor-4.png" alt="sponsor" />
+        <img src="/sponsor-5.png" alt="sponsor" />
+      </div>
+    </section>
   );
 };
 
---
+++ src/section-2.jsx
@@ -0,0 +1,13 @@
+const SectionTwo = () => {
+  return (
+    <section className="section-2">
+      <div className="section-2-box ">
+        <p className="section-2-title">Our objective is straightforward</p>
+        <p className="section-2-subtitle-1">To assist Business</p>
+        <p className="section-2-subtitle-2">Generate greater profits</p>
+      </div>
+    </section>
+  );
+};
+
+export default SectionTwo;
